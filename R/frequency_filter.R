63a664b4881a991cddcf8f030012a30e6c0dc0d7
Rose Maier <rosemhartman@gmail.com>
2017-11-08 00:22:16 -0500

    adding documentation for frequency_filter()

--- R/frequency_filter.R
+++ R/frequency_filter.R
@@ -1,13 +1,40 @@
+#' Residualizes child counts by regressing on adult counts for the same words
+#'
+#' Residualizes child word counts by regressing on counts for the main adult interlocotur for the same words in the same transcripts.
+#' The input data should already be word counts by speaker for each transcript or timepoint. 
+#' By default, counts are log transformed before regressing.
+#'
+#' @param data_input the dataframe of counts in child and adult speech
+#' @param chi.count the name of the column with child counts
+#' @param adu.count the name of the column with adult counts
+#' @param min.types the minimum number of types necessary to run regression at a given timepoint or transcript. If set to 0, it will attempt to fit a regression for every timepoint/transcript regardless of how many words are available. Note that it is inadvisable to use residuals from regressions fit at very sparse timepoints.
+#' @param log log transform counts before regressing? Default is TRUE.
+#' @param base the base to use for log transformation
+#' @param drop.zeros should zero-count words (words that never occur in adult speech, or never in child speech) be excluded? Default is TRUE, which restricts analysis to counts of words that are observed in both child and adult speech.
+#' @param outlier_cutoff how should outliers (exceptionally high or low residuals) be defined? Default is 1 SD. 
+#' @param debug turn on to display extra messages while the function runs, useful for identifying problem transcripts. Default is FALSE. 
+#' 
+#' @return The original input data, with new variables added on providing results from the regressions including residual values for each word. 
+#'
+#' @examples
+#' frequency_filter(data_pos)
+#' 
+#' @references Ibbotson, Hartman, and Nilsson Björkenstam. (in prep). "Frequency Filter: An Open Access Tool for Analysing Language Development".
+#' 
 #' @export
-frequency_filter <- function(data_input, log=TRUE, base = exp(1), min.types=200, drop.zeros=log, outlier_cutoff=1, debug = FALSE){
+frequency_filter <- function(data_input, chi.count = "chi.count", adu.count = "adu.count", min.types=200, log=TRUE, base = exp(1), drop.zeros=TRUE, outlier_cutoff=1, debug = FALSE){
   if(debug) message(unique(data_input$id))
+  
+  # for use in dplyr functions below (see vignette("programming") in the dplyr package)
+  chi.count <- enquo(chi.count)
+  adu.count <- enquo(adu.count)
   
   # For each word, get sum of chi counts and adu counts.
   # Note that in most cases, this will already be accomplished, 
   # but words that have more than one gloss used at different points in the transcription
   # may have separate counts. This ensures that cases like that are properly collapsed.
   data <- data_input %>% 
-    dplyr::select(word, chi.count, adu.count) %>% 
+    dplyr::select(word, chi.count = !!chi.count, adu.count=!!adu.count) %>% 
     group_by(word) %>%
     summarise(chi.count = sum(chi.count), 
               adu.count = sum(adu.count))
