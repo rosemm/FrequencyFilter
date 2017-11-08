d28e1f795f2a27d08c5a455b87011109330ce68b
Rose Maier <rosemhartman@gmail.com>
2017-11-07 21:15:09 -0500

    separated counts of words and utts into two dataframes, which are then joined

--- R/bin_ages.R
+++ R/bin_ages.R
@@ -1,30 +1,35 @@
 #' @export
-bin_ages <- function(data, breaks, use.mor=FALSE){
-
+bin_ages <- function(data, breaks){
+# define a function that pools transcripts for a given child by approximate age
+  
   data$age.bin <- cut(data$age.mos, breaks=breaks, labels=FALSE) # break age into bins
+  
   age.bins <- data %>%
     group_by(age.bin) %>%
-    summarize(age=round(mean(age.mos, na.rm=TRUE), 0))
+    # age is the average age for this child's files within that window
+    dplyr::summarize(age=round(mean(age.mos, na.rm=TRUE), 0))
+  
   data <- left_join(data, age.bins, by="age.bin")
 
   data$age.bin <- NULL # drop that column
 
   # pool all of the transcript counts within each age bin for each child
-  if(!use.mor){
-    data_grouped <- data %>%
-      group_by(child, age, word)
-  }
-  if(use.mor){
-    data_grouped <- data %>%
-      group_by(child, age, orth_word, POS, word)
-  }
-
-  data <- data_grouped %>%
+  # sum counts for each word in chi and adu speech
+  pooled_word_counts <- data %>%
+    group_by(child, age, word, gloss, POS) %>% # note that if mor was not use in count_words(), POS will just be NA
     summarize(chi.count=sum(chi.count, na.rm = TRUE),
-              adu.count=sum(adu.count, na.rm = TRUE),
-              N.chi.utts=sum(N.chi.utts, na.rm = TRUE),
-              N.adu.utts=sum(N.adu.utts, na.rm = TRUE)) %>%
-    unite(col="id", child, age, remove=FALSE)
-
-  return(data)
-} # define a function that pools transcripts for a given child by approximate age
+              adu.count=sum(adu.count, na.rm = TRUE)) 
+  # get totals for chi and adu utterances from all files in age bin
+  pooled_utt_counts <- data %>%
+    dplyr::select(child, age, file, N.chi.utts, N.adu.utts) %>% 
+    unique() %>% 
+    group_by(child, age) %>% 
+    summarize(N.chi.utts=sum(N.chi.utts, na.rm = TRUE),
+              N.adu.utts=sum(N.adu.utts, na.rm = TRUE)) 
+  
+  data %>% 
+    dplyr::select(child, age, word, gloss, POS) %>% 
+    unique() %>% 
+    left_join(pooled_word_counts, by = c("child", "age", "word", "gloss", "POS")) %>% 
+    left_join(pooled_utt_counts, by = c("child", "age"))
+} 
